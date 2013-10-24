eb79c4a30365f5485dfc71f69b449c0716f1c0f3
Ryan Tablada <ryan.tablada@gmail.com>
2013-10-24 16:09:49 -0500

    Update year and month pickers to use the query stringer

--- src/views/month.php
+++ src/views/month.php
@@ -1,17 +1,17 @@
 <div class="pagination">
 	<ul>
 		<?php if ($previous) : ?>
-		<li><a href="?month=<?= $previous->month?>">&laquo;</a></li>
+		<li><a href="<?= $previous->getQueryString() ?>">&laquo;</a></li>
 		<?php endif ?>
 		<?php foreach ($months as $month) : ?>
 			<?php if ($month->display != 'hide') : ?>
 			<li <?= $month->display ? 'class="'.$month->display.'"' : null ?>>
-				<a href="?month=<?= $month->month?>"><?= $month->month ?></a>
+				<a href="<?= $month->getQueryString() ?>"><?= $month->month ?></a>
 			</li>
 			<?php endif ?>
 		<?php endforeach ?>
 		<?php if ($next) : ?>
-		<li><a href="?month=<?= $next->month?>">&raquo;</a></li>
+		<li><a href="<?= $next->getQueryString() ?>">&raquo;</a></li>
 		<?php endif ?>
 	</ul>
 </div>
